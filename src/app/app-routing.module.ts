f4cdce58b0384be9957086ec78c15b4d93e10f95
Mauricio Murua <murua5@hotmail.com>
2023-10-24 10:18:30 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { UsuarioGuard } from './guards/usuario.guard';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  {
+    path: 'main',
+    loadChildren: './pages/tabs/tabs.module#TabsPageModule',
+    // canActivate: [ UsuarioGuard ]
+    canLoad: [ UsuarioGuard ]
+  },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'main/tabs/tab1'
+  }
 ];
 @NgModule({
   imports: [
